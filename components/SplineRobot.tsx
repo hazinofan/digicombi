262f11a2e822ccd1bc57f69467d50cba90f56c86
Amine YOUSSSOUFI <devops@t2mc.fr>
2025-08-27 14:31:41 +0100

    build failing fix

--- components/SplineRobot.tsx
+++ components/SplineRobot.tsx
@@ -1,19 +1,16 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import type { ComponentType } from "react";
 
-const Spline = dynamic(() => import("@splinetool/react-spline"), { ssr: false });
+const Spline = dynamic(
+  () => import("@splinetool/react-spline").then((m) => m.default),
+  { ssr: false }
+) as ComponentType<{ scene: string; className?: string }>;
 
 export default function SplineRobot({
   sceneUrl,
   className,
-}: {
-  sceneUrl: string;
-  className?: string;
-}) {
-  return (
-    <div className={className}>
-      <Spline scene={sceneUrl} />
-    </div>
-  );
+}: { sceneUrl: string; className?: string }) {
+  return <Spline scene={sceneUrl} className={className} />;
 }
